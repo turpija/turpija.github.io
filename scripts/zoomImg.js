e120f9f1c39a316c39d0f0332c8d94ae3d8eb3f8
turpija <turpija@gmail.com>
2023-02-13 17:45:06 +0100

    asdf

---
+++ scripts/zoomImg.js
@@ -0,0 +1,80 @@
+function createDuplicateImg(origImg) {
+    // console.dir(origImg);
+    const img = new Image();
+    img.src = origImg.src;
+    img.style.filter = origImg.style.filter;
+    img.style.position = "absolute";
+    img.style.zIndex = "50";
+
+    const paddingLeft = parseInt(window.getComputedStyle(origImg).paddingLeft);
+    const paddingTop = parseInt(window.getComputedStyle(origImg).paddingTop);
+    const origImgLeft = origImg.getBoundingClientRect().left;
+    const origImgTop = origImg.getBoundingClientRect().top;
+    const scrollTopOffset = window.pageYOffset;
+
+    img.style.left = `${origImgLeft+paddingLeft}px`;
+    img.style.top = `${origImgTop+paddingTop+scrollTopOffset}px`;
+    // console.log("boundingRect: ", origImg.getBoundingClientRect());
+    // console.log("Yoffset: ", window.pageYOffset);
+
+    const body = document.querySelector("body");
+    body.append(img);
+    return img;
+}
+
+function resizeToFullScreen(newImg,linkStr) {
+    console.dir("passed through url:",linkStr);
+    const prop = newImg.style.width;
+    newImg.style.left = "0px";
+    newImg.style.top = `${window.pageYOffset}px`;
+    newImg.style.width = `${document.body.clientWidth}px`;
+
+    function afterImageScale(e) {
+        newImg.removeEventListener("transitionend", afterImageScale);
+        console.log("after img resize ... do some tricks")
+        window.setTimeout(() => {
+            newImg.style.display = "none";
+        }, 500);
+    };
+
+    newImg.addEventListener("transitionend", afterImageScale);
+
+}
+
+function clickedImage(e) {
+    e.preventDefault();
+    let origImg = e.target;
+    let urlStr = origImg.dataset.url;
+    console.log(urlStr);
+    
+    function origImgWidth() {
+        const origImgWidthFull = parseInt(getComputedStyle(origImg).width);
+        const origImgPaddingLeft = parseInt(getComputedStyle(origImg).paddingLeft)
+        const origImgPaddingRight = parseInt(getComputedStyle(origImg).paddingRight);
+        
+        return origImgWidthFull - origImgPaddingLeft - origImgPaddingRight;
+    }
+    
+    // console.log(origImgWidth());
+    newImg = createDuplicateImg(e.target);
+    newImg.style.width = `${origImgWidth()}px`;
+    newImg.style.transition = "all .3s ease-in-out .5s";
+    newImg.url = origImg.dataset.url;
+    window.setTimeout(() => {
+        resizeToFullScreen(newImg,urlStr)
+    }, 20);
+}
+
+function addClickEvents() {
+    const images = document.querySelectorAll(".zoomImg");
+    // console.log(images);
+
+    images.forEach((image) => {
+        image.addEventListener("click", clickedImage)
+    })
+}
+
+addClickEvents();
+
+// const cardLeft = document.querySelector(".card .left");
+// console.log("aktualy širina:",getComputedStyle(cardLeft).width);
