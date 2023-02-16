ff08b3eb1ca698b370b4e17b69b828e2264883c9
turpija <turpija@gmail.com>
2023-02-16 10:28:35 +0100

    done

--- odin-signup-form/script.js
+++ odin-signup-form/script.js
@@ -1,1 +1,10 @@
-const passwordInput = document.querySelector("#")
+const passwordInput = document.querySelector("#password");
+const passwordConfirmInput = document.querySelector("#passConfirm");
+const errorMsg = document.querySelector(".formLayout .errorMsg");
+
+function passInvalid () {
+    passwordInput.classList.add("error");
+    passwordConfirmInput.classList.add("error");
+    errorMsg.style.visibility = "visible";
+}
+passInvalid();
